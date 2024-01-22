3f115170015bda3081003abbc6ae68f1a57923bb
Marina <mar.radchenko@gmail.com>
2024-01-22 14:42:56 +0200

    get currency rates via http

---
+++ src/Services/CurrencyEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+
+enum CurrencyEnum: string {
+    case USD = 'USD';
+    case EUR = 'EUR';
+}
---
+++ src/Services/CurrencyRatesLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CurrencyRatesLoader
+{
+    public function __construct(
+        protected HttpClientInterface $client,
+    ) {}
+
+    public function load(): ?array
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.freecurrencyapi.com/v1/latest',
+            [
+                'query' => [
+                    'apikey' => 'fca_live_pfc87jPfmouVPcDUpl32XkK3D6f7BeupOjtLYJXl',
+                    'base_currency' => CurrencyEnum::USD->value,
+                    'currencies' => CurrencyEnum::EUR->value,
+                ],
+            ]
+        );
+
+        $statusCode = $response->getStatusCode();
+        if ($statusCode !== 200) {
+            throw new \Exception("Wrong status code from exchanger");
+        }
+
+        $content = $response->toArray();
+        if (!isset($content['data'])) {
+            throw new \Exception("Wrong data structure from exchanger");
+        }
+
+        return $content['data'];
+    }
+}
